96e6c67da927b57d50405323ea27973fd8841384
rsankar09 <rajan.sankara@gmail.com>
2026-07-24 10:38:21 -0500

    cards ue json changes

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -18,5 +18,39 @@ export default function decorate(block) {
   rawImageContainer.remove();
   block.prepend(imageWrapper);
 
-  
+  // --- CONTENT WRAPPER ---
+  const contentWrapper = document.createElement('div');
+  contentWrapper.classList.add('content-wrapper');
+
+  // Move all remaining children into content wrapper
+  while (block.children.length > 1) {
+    contentWrapper.appendChild(block.children[1]);
+  }
+
+  block.appendChild(contentWrapper);
+
+  // --- FLATTEN nested divs ---
+  contentWrapper.querySelectorAll('div').forEach((div) => {
+    if (div.children.length === 1 && div.children[0].tagName === 'DIV') {
+      div.replaceWith(div.children[0]);
+    }
+  });
+
+  // Normalize title
+  const title = contentWrapper.querySelector('h2, p');
+  if (title) title.classList.add('teaser-title');
+
+  // Normalize subtitle
+  const subtitle = contentWrapper.querySelector('.teaser-subtitle');
+  if (!subtitle) {
+    const p = contentWrapper.querySelector('p:nth-of-type(2)');
+    if (p) p.classList.add('teaser-subtitle');
+  }
+
+  // Normalize description
+  const desc = contentWrapper.querySelector('.teaser-description');
+  if (!desc) {
+    const p = contentWrapper.querySelector('p:nth-of-type(3)');
+    if (p) p.classList.add('teaser-description');
+  }
 }
