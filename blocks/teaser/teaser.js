12afed64db095a5fa426866bdd654311f361b8e8
rsankar09 <rajan.sankara@gmail.com>
2026-07-27 09:39:56 -0500

    cards ue json changes

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -1,92 +1,71 @@
-// export default function decorate(block) {
-//   console.log('Teaser block decoration ');
-//   console.log('Block:',block);
-//   block.classList.add('teaser');  
-//   console.log(block.dataset);
-//   console.log(block.dataset.style);
-//   const styleClass = block.dataset.style;
-//   if (styleClass) block.classList.add(styleClass);
-
-//   const rows = [...block.children];
-
-//   const getCol = (row) => row?.children?.[0] || row;
-
-//   const imageCol = getCol(rows[0]);
-//   const titleCol = getCol(rows[1]);
-//   const subtitleCol = getCol(rows[2]);
-//   const descCol = getCol(rows[3]);
-//   const ctaCol = getCol(rows[4]);
-//   console.log(imageCol, titleCol, subtitleCol, descCol, ctaCol);    
-
-
-//   const picture = imageCol?.querySelector('picture');
-// //   const title = titleCol?.querySelector('h1, h2, h3') || block.querySelector('h1, h2, h3')
-//   const title = titleCol?.querySelector('p'); 
-//   const subtitle = subtitleCol?.querySelector('p');
-//   const description = descCol?.querySelector('p');
-//   const cta = ctaCol?.querySelector('p');
-
-//   block.innerHTML = '';
-
-//   // TITLE
-//   if (title) {
-//     title.classList.add('teaser-title');
-//     block.appendChild(title);
-//   }
-
-//   const body = document.createElement('div');
-//   body.className = 'teaser-body';
-
-//   // IMAGE WRAPPER
-//   if (picture) {
-//     const imageWrapper = document.createElement('div');
-//     imageWrapper.className = 'image-wrapper';
-//     imageWrapper.appendChild(picture);
-//     body.appendChild(imageWrapper);
-//   }
-
-//   // TEXT WRAPPER
-//   const textWrapper = document.createElement('div');
-//   textWrapper.className = 'text-wrapper';
-
-//   if (subtitle) {
-//     subtitle.classList.add('teaser-subtitle');
-//     textWrapper.appendChild(subtitle);
-//   }
-
-//   if (description) {
-//     description.classList.add('teaser-description');
-//     textWrapper.appendChild(description);
-//   }
-
-//   if (cta) {
-//     const ctaP = document.createElement('p');
-//     ctaP.classList.add('teaser-cta');
-//     ctaP.appendChild(cta);
-//     textWrapper.appendChild(ctaP);
-//   }
-
-//   body.appendChild(textWrapper);
-//   block.appendChild(body);
-// }
-
-import { createOptimizedPicture } from '../../scripts/aem.js';
-
 export default function decorate(block) {
-  /* change to ul, li */
-  const ul = document.createElement('ul');
-  [...block.children].forEach((row) => {
-    console.log(row);
-    // const li = document.createElement('li');
-    // while (row.firstElementChild) li.append(row.firstElementChild);
-    // [...li.children].forEach((div) => {
-    //     console.log(div);
-    // });
-    // ul.append(li);
-  });
+  console.log('Teaser block decoration ');
+  console.log('Block:',block);
+  block.classList.add('teaser');  
+  console.log(block.dataset);
+  console.log(block.dataset.style);
+  const styleClass = block.dataset.style;
+  if (styleClass) block.classList.add(styleClass);
 
-  // replace images with optimized versions
-  ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  const rows = [...block.children];
 
-  block.replaceChildren(ul);
+  const getCol = (row) => row?.children?.[0] || row;
+
+  const imageCol = getCol(rows[0]);
+  const titleCol = getCol(rows[1]);
+  const subtitleCol = getCol(rows[2]);
+  const descCol = getCol(rows[3]);
+  const ctaCol = getCol(rows[4]);
+  console.log(imageCol, titleCol, subtitleCol, descCol, ctaCol);    
+
+
+  const picture = imageCol?.querySelector('picture');
+//   const title = titleCol?.querySelector('h1, h2, h3') || block.querySelector('h1, h2, h3')
+  const title = titleCol?.querySelector('p'); 
+  const subtitle = subtitleCol?.querySelector('p');
+  const description = descCol?.querySelector('p');
+  const cta = ctaCol?.querySelector('p');
+
+  block.innerHTML = '';
+
+  // TITLE
+  if (title) {
+    title.classList.add('teaser-title');
+    block.appendChild(title);
+  }
+
+  const body = document.createElement('div');
+  body.className = 'teaser-body';
+
+  // IMAGE WRAPPER
+  if (picture) {
+    const imageWrapper = document.createElement('div');
+    imageWrapper.className = 'image-wrapper';
+    imageWrapper.appendChild(picture);
+    body.appendChild(imageWrapper);
+  }
+
+  // TEXT WRAPPER
+  const textWrapper = document.createElement('div');
+  textWrapper.className = 'text-wrapper';
+
+  if (subtitle) {
+    subtitle.classList.add('teaser-subtitle');
+    textWrapper.appendChild(subtitle);
+  }
+
+  if (description) {
+    description.classList.add('teaser-description');
+    textWrapper.appendChild(description);
+  }
+
+  if (cta) {
+    const ctaP = document.createElement('p');
+    ctaP.classList.add('teaser-cta');
+    ctaP.appendChild(cta);
+    textWrapper.appendChild(ctaP);
+  }
+
+  body.appendChild(textWrapper);
+  block.appendChild(body);
 }
