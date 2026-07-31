6bd7f6794d3797a8d05aa59e8741633eddfbb729
rsankar09 <rajan.sankara@gmail.com>
2026-07-31 10:34:25 -0500

    cards ue json changes

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,5 +1,5 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
-import { moveInstrumentation } from '../../scripts/scripts.js';
+// import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
 
@@ -31,7 +31,7 @@ export default function decorate(block) {
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
-    moveInstrumentation(row, li);
+    // moveInstrumentation(row, li);
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
